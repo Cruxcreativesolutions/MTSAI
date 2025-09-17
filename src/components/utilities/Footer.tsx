89358b6adb601890af44ac97c7b7839fa729e620
lakshay-crux <lakshaybhati@cruxcreativesolutions.com>
2025-09-17 13:31:11 +0530

    navbar and footer

---
+++ src/components/utilities/Footer.tsx
@@ -0,0 +1,399 @@
+import {
+  Facebook,
+  Twitter,
+  Linkedin,
+  Instagram,
+  Phone,
+  Mail,
+  MapPin,
+} from "lucide-react";
+import Image from "next/image";
+import appstore from "@/assets/footer/appstore.webp";
+import playstore from "@/assets/footer/playstore.webp";
+
+function MiracleLogo() {
+  return (
+    <svg
+      width="128"
+      height="44"
+      viewBox="0 0 128 44"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clipPath="url(#clip0_540_42635)">
+        <path
+          d="M39.2079 10.0948C31.0257 10.0948 24.3973 16.7278 24.3973 24.9054H15.3152C15.3152 16.7232 8.68224 10.0948 0.5 10.0948V1.0127C8.46887 1.0127 15.5147 4.909 19.8563 10.8972C24.1979 4.91364 31.2437 1.0127 39.2079 1.0127V10.0948Z"
+          fill="white"
+        />
+        <path
+          d="M53.6846 15.0582H57.7015C56.5187 7.11251 49.6723 1.01758 41.3973 1.01758V10.0997C47.1861 10.0997 48.9766 13.5182 49.3708 17.0713C50.4052 15.8421 51.9545 15.0582 53.6846 15.0582Z"
+          fill="#53AA00"
+        />
+        <path
+          d="M57.901 20.689V16.4727H53.6846C51.3561 16.4727 49.4683 18.3605 49.4683 20.689C49.4683 23.0175 51.3561 24.9054 53.6846 24.9054C56.0131 24.9054 57.901 23.0175 57.901 20.689ZM54.9277 20.689C54.9277 21.3755 54.3711 21.9321 53.6846 21.9321C52.9981 21.9321 52.4415 21.3755 52.4415 20.689C52.4415 20.0025 52.9981 19.4459 53.6846 19.4459C54.3711 19.4459 54.9277 20.0025 54.9277 20.689Z"
+          fill="#D82B2B"
+        />
+        <path
+          d="M122.83 0.808594C120.372 0.808594 118.377 2.80313 118.377 5.26151C118.377 7.7199 120.372 9.71443 122.83 9.71443C125.289 9.71443 127.283 7.7199 127.283 5.26151C127.283 2.80313 125.289 0.808594 122.83 0.808594ZM122.83 6.50926C122.144 6.50926 121.587 5.95264 121.587 5.26615C121.587 4.57966 122.144 4.02305 122.83 4.02305C123.517 4.02305 124.073 4.57966 124.073 5.26615C124.073 5.95264 123.517 6.50926 122.83 6.50926Z"
+          fill="#D82B2B"
+        />
+        <path
+          d="M65.2899 1.01758V10.0997H57.8313C55.5585 5.4705 52.5574 2.04732 47.3623 1.01758H65.2946H65.2899Z"
+          fill="white"
+        />
+        <path
+          d="M0.509277 11.7598C3.93246 11.7598 7.09125 12.9611 9.5821 14.9603V24.9005H0.5V11.7598H0.509277Z"
+          fill="white"
+        />
+        <path
+          d="M39.2126 11.7598V24.9005H30.1305V14.9603C32.6213 12.9611 35.7801 11.7598 39.2033 11.7598H39.2126Z"
+          fill="white"
+        />
+        <path
+          d="M68.1009 15.8232H64.6777V24.9053H81.2649C75.1143 23.8849 71.756 20.5452 68.0963 15.8232H68.1009Z"
+          fill="#53AA00"
+        />
+        <path
+          d="M75.425 1.01758C75.425 4.45004 76.6264 7.60883 78.6349 10.0904H88.5751V1.01758H75.425Z"
+          fill="#53AA00"
+        />
+        <path
+          d="M81.8539 14.2042C79.4883 12.8127 77.4659 11.1939 76.0001 8.83753C75.2765 7.67792 74.7153 6.42089 74.3396 5.10821C74.2422 4.77424 73.5464 1.00781 73.7644 1.00781C71.4313 1.00781 69.1955 2.15815 67.8179 4.04136C64.8864 8.03508 67.3355 12.757 70.0304 16.0689C72.2383 18.7824 75.04 21.0042 78.1895 22.5303C79.7944 23.3049 81.4828 23.8986 83.2176 24.2975C83.6582 24.3996 88.5796 25.1278 88.5796 24.9005V24.4459C88.5796 22.2334 87.8885 20.0533 86.6315 18.2304C85.4116 16.4539 83.6907 15.2803 81.8585 14.2042H81.8539Z"
+          fill="#53AA00"
+        />
+        <path
+          d="M115.868 11.6953V25.0123H106.665V14.9376C109.188 12.9152 112.389 11.6953 115.858 11.6953H115.868Z"
+          fill="white"
+        />
+        <path
+          d="M118.229 11.6953V25.0123H127.431V14.9376C124.908 12.9152 121.708 11.6953 118.238 11.6953H118.229Z"
+          fill="white"
+        />
+        <path
+          d="M96.2564 10.8277C96.2332 10.7998 91.655 16.5562 91.655 25.0167H100.858C100.858 16.7278 107.574 10.0113 115.863 10.0113V0.808594C107.797 0.808594 100.654 4.76056 96.2564 10.8277Z"
+          fill="#53AA00"
+        />
+        <path
+          d="M0.690186 34.8689H2.50846L4.82305 40.5464L5.10136 41.4138L5.37967 40.5464L7.67106 34.8689H9.44296V43.0651H8.09316L8.13027 36.4228L7.54119 38.1808L5.55129 43.0604H4.58649L2.58731 38.1668L2.00751 36.4552L2.04462 43.0604H0.690186V34.8643V34.8689Z"
+          fill="white"
+        />
+        <path
+          d="M11.9199 34.8691H13.2697V43.0653H11.9199V34.8691Z"
+          fill="white"
+        />
+        <path
+          d="M15.7512 34.8689H18.8265C20.5984 34.8689 21.7209 35.7827 21.7209 37.2299C21.7209 38.1576 21.2988 38.8069 20.5659 39.1687C21.0483 39.4239 21.3127 39.8692 21.4936 40.602L22.1105 43.0604H20.6865L20.1902 40.8664C19.9722 39.9248 19.6104 39.7439 18.7059 39.7439H17.101V43.0604H15.7512V34.8643V34.8689ZM17.101 35.9543V38.6678H18.7662C19.7681 38.6678 20.334 38.139 20.334 37.3041C20.334 36.4692 19.7681 35.9543 18.7894 35.9543H17.101Z"
+          fill="white"
+        />
+        <path
+          d="M29.1656 41.2231H25.427L24.7266 43.0692H23.3768L26.5356 34.873H28.1127L31.2715 43.0692H29.8614L29.161 41.2231H29.1656ZM25.8352 40.1609H28.7528L27.5375 36.965L27.2963 36.1672L27.0551 36.965L25.8399 40.1609H25.8352Z"
+          fill="white"
+        />
+        <path
+          d="M38.5864 37.3041C38.3081 36.3625 37.5242 35.8476 36.5501 35.8476C35.0565 35.8476 34.1613 37.0397 34.1613 38.9832C34.1613 40.9267 35.0426 42.0956 36.5501 42.0956C37.4917 42.0956 38.2478 41.6132 38.5632 40.6855H39.9872C39.5419 42.3136 38.1689 43.2274 36.5501 43.2274C34.2123 43.2274 32.7883 41.6225 32.7883 38.9832C32.7883 36.3439 34.2216 34.7158 36.5872 34.7158C38.2153 34.7158 39.6115 35.6064 39.9965 37.3087H38.5864V37.3041Z"
+          fill="white"
+        />
+        <path
+          d="M42.0698 34.8691H43.4196V41.9706H47.3855V43.0653H42.0698V34.8691Z"
+          fill="white"
+        />
+        <path
+          d="M49.3477 34.8691H55.0019V35.9545H50.6974V38.2923H54.5427V39.3638H50.6974V41.9799H55.0019V43.0653H49.3477V34.8691Z"
+          fill="white"
+        />
+        <path
+          d="M62.3956 36.0009H59.6589V34.8691H66.4682V36.0009H63.7454V43.0653H62.3956V36.0009Z"
+          fill="white"
+        />
+        <path
+          d="M68.4209 34.8689H71.4962C73.2681 34.8689 74.3906 35.7827 74.3906 37.2299C74.3906 38.1576 73.9685 38.8069 73.2356 39.1687C73.718 39.4239 73.9824 39.8692 74.1633 40.602L74.7802 43.0604H73.3562L72.8599 40.8664C72.6419 39.9248 72.2801 39.7439 71.3756 39.7439H69.7707V43.0604H68.4209V34.8643V34.8689ZM69.7707 35.9543V38.6678H71.4359C72.4378 38.6678 73.0037 38.139 73.0037 37.3041C73.0037 36.4692 72.4378 35.9543 71.4591 35.9543H69.7707Z"
+          fill="white"
+        />
+        <path
+          d="M81.8354 41.2231H78.0968L77.3964 43.0692H76.0466L79.2054 34.873H80.7825L83.9413 43.0692H82.5312L81.8308 41.2231H81.8354ZM78.5097 40.1609H81.4272L80.212 36.965L79.9708 36.1672L79.7296 36.965L78.5143 40.1609H78.5097Z"
+          fill="white"
+        />
+        <path
+          d="M85.7502 34.8691H91.4045V35.9545H87.1V38.5335H90.9453V39.6189H87.1V43.0653H85.7502V34.8691Z"
+          fill="white"
+        />
+        <path
+          d="M93.413 34.8691H99.0672V35.9545H94.7628V38.5335H98.608V39.6189H94.7628V43.0653H93.413V34.8691Z"
+          fill="white"
+        />
+        <path
+          d="M101.076 34.8691H102.425V43.0653H101.076V34.8691Z"
+          fill="white"
+        />
+        <path
+          d="M110.418 37.3041C110.139 36.3625 109.355 35.8476 108.381 35.8476C106.888 35.8476 105.992 37.0397 105.992 38.9832C105.992 40.9267 106.874 42.0956 108.381 42.0956C109.323 42.0956 110.079 41.6132 110.394 40.6855H111.818C111.373 42.3136 110 43.2274 108.381 43.2274C106.044 43.2274 104.62 41.6225 104.62 38.9832C104.62 36.3439 106.053 34.7158 108.418 34.7158C110.047 34.7158 111.443 35.6064 111.828 37.3087H110.418V37.3041Z"
+          fill="white"
+        />
+        <path
+          d="M122.083 41.2231H118.345L117.644 43.0692H116.294L119.453 34.873H121.03L124.189 43.0692H122.779L122.079 41.2231H122.083ZM118.753 40.1609H121.67L120.455 36.965L120.214 36.1672L119.973 36.965L118.757 40.1609H118.753Z"
+          fill="white"
+        />
+        <path
+          d="M125.998 34.8691H127.348V43.0653H125.998V34.8691Z"
+          fill="white"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_540_42635">
+          <rect
+            width="126.931"
+            height="42.4141"
+            fill="white"
+            transform="translate(0.5 0.808594)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
+
+export default function Footer() {
+  return (
+    <footer
+      className="relative bg-gradient-to-r from-blue-600 via-blue-700 to-slate-900 text-white"
+      style={{
+        backgroundColor: "#0052CC",
+        background:
+          "linear-gradient(135deg, rgba(0, 82, 204, 1) 15%, rgba(0, 9, 23, 1) 90%)",
+      }}
+    >
+      <div className="container mx-auto px-6 py-12">
+        {/* Desktop Layout */}
+        <div className="hidden lg:grid lg:grid-cols-3 lg:gap-12 lg:items-start">
+          {/* Left Section - Logo and App Downloads */}
+          <div className="space-y-6">
+            <div className="flex items-center">
+              <MiracleLogo />
+            </div>
+
+            {/* Download Text */}
+            <div className="space-y-3">
+              <div className="text-base">
+                Download the app by clicking the link below :
+              </div>
+
+              <div className="flex space-x-4 items-center">
+                <Image
+                  src={playstore}
+                  alt="Get it on Google Play"
+                  width={160}
+                  height={48}
+                  className="h-16 w-auto"
+                />
+                <Image
+                  src={appstore}
+                  alt="Download on the App Store"
+                  width={160}
+                  height={48}
+                  className="h-12 w-auto"
+                />
+              </div>
+            </div>
+
+            {/* Social Media Icons */}
+            <div className="flex space-x-4">
+              <Facebook className="w-6 h-6 text-white hover:text-blue-300 cursor-pointer" />
+              <Twitter className="w-6 h-6 text-white hover:text-blue-300 cursor-pointer" />
+              <Linkedin className="w-6 h-6 text-white hover:text-blue-300 cursor-pointer" />
+              <Instagram className="w-6 h-6 text-white hover:text-blue-300 cursor-pointer" />
+            </div>
+          </div>
+
+          {/* Center Section - Pages */}
+          <div className="space-y-6">
+            <h3 className="text-xl font-semibold">Pages</h3>
+            <div className="grid grid-cols-2 gap-4">
+              <div className="space-y-3">
+                <a
+                  href="#"
+                  className="block text-gray-200 hover:text-white transition-colors"
+                >
+                  About Us
+                </a>
+                <a
+                  href="#"
+                  className="block text-gray-200 hover:text-white transition-colors"
+                >
+                  Technology
+                </a>
+                <a
+                  href="#"
+                  className="block text-gray-200 hover:text-white transition-colors"
+                >
+                  Solutions
+                </a>
+                <a
+                  href="#"
+                  className="block text-gray-200 hover:text-white transition-colors"
+                >
+                  Government Portal
+                </a>
+              </div>
+              <div className="space-y-3">
+                <a
+                  href="#"
+                  className="block text-gray-200 hover:text-white transition-colors"
+                >
+                  Investor Centre
+                </a>
+                <a
+                  href="#"
+                  className="block text-gray-200 hover:text-white transition-colors"
+                >
+                  Resources
+                </a>
+                <a
+                  href="#"
+                  className="block text-gray-200 hover:text-white transition-colors"
+                >
+                  Contact Us
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* Right Section - Contact */}
+          <div className="space-y-6">
+            <h3 className="text-xl font-semibold">Contact</h3>
+            <div className="space-y-4">
+              <div className="flex items-center space-x-3">
+                <Phone className="w-5 h-5 text-white" />
+                <span className="text-gray-200">(406) 555-0120</span>
+              </div>
+              <div className="flex items-center space-x-3">
+                <Mail className="w-5 h-5 text-white" />
+                <span className="text-gray-200">ssq@miracletraffic.ai</span>
+              </div>
+              <div className="flex items-start space-x-3">
+                <MapPin className="w-5 h-5 text-white mt-1 flex-shrink-0" />
+                <span className="text-gray-200 leading-relaxed">
+                  511 Rupa Solitaire, Millennium Business Park, Mahape,
+                  Ghansoli, Navi Mumbai, Maharashtra 400710
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Mobile Layout */}
+        <div className="lg:hidden space-y-8 text-center">
+          <div className="flex items-center justify-center">
+            <MiracleLogo />
+          </div>
+
+          {/* Download Text */}
+          <div className="space-y-3">
+            <div className="text-base">
+              Download the app by clicking the link below :
+            </div>
+
+            <div className="flex space-x-4 items-center justify-center">
+              <Image
+                src={playstore}
+                alt="Get it on Google Play"
+                width={160}
+                height={48}
+                className="h-14 w-auto"
+              />
+              <Image
+                src={appstore}
+                alt="Download on the App Store"
+                width={160}
+                height={48}
+                className="h-10 w-auto"
+              />
+            </div>
+          </div>
+
+          {/* Divider */}
+          <div className="border-t border-blue-400 opacity-30"></div>
+
+          {/* Pages Section */}
+          <div className="space-y-6">
+            <h3 className="text-xl font-semibold">Pages</h3>
+            <div className="grid grid-cols-2 gap-4 text-left max-w-md mx-auto">
+              <div className="space-y-4">
+                <a
+                  href="#"
+                  className="block text-gray-200 hover:text-white transition-colors"
+                >
+                  About Us
+                </a>
+                <a
+                  href="#"
+                  className="block text-gray-200 hover:text-white transition-colors"
+                >
+                  Technology
+                </a>
+                <a
+                  href="#"
+                  className="block text-gray-200 hover:text-white transition-colors"
+                >
+                  Solutions
+                </a>
+                <a
+                  href="#"
+                  className="block text-gray-200 hover:text-white transition-colors"
+                >
+                  Government Portal
+                </a>
+              </div>
+              <div className="space-y-4">
+                <a
+                  href="#"
+                  className="block text-gray-200 hover:text-white transition-colors"
+                >
+                  Investor Centre
+                </a>
+                <a
+                  href="#"
+                  className="block text-gray-200 hover:text-white transition-colors"
+                >
+                  Resources
+                </a>
+                <a
+                  href="#"
+                  className="block text-gray-200 hover:text-white transition-colors"
+                >
+                  Contact Us
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* Divider */}
+          <div className="border-t border-blue-400 opacity-30"></div>
+
+          {/* Contact Section */}
+          <div className="space-y-6">
+            <h3 className="text-xl font-semibold">Contact</h3>
+            <div className="space-y-6">
+              <div className="flex items-center justify-center space-x-3">
+                <Phone className="w-6 h-6 text-white" />
+                <span className="text-gray-200 text-lg">(406) 555-0120</span>
+              </div>
+              <div className="flex items-center justify-center space-x-3">
+                <Mail className="w-6 h-6 text-white" />
+                <span className="text-gray-200 text-lg">
+                  ssq@miracletraffic.ai
+                </span>
+              </div>
+              <div className="flex items-start justify-center space-x-3 max-w-sm mx-auto">
+                <MapPin className="w-6 h-6 text-white mt-1 flex-shrink-0" />
+                <span className="text-gray-200 leading-relaxed text-center">
+                  511 Rupa Solitaire, Millennium Business Park, Mahape,
+                  Ghansoli, Navi Mumbai, Maharashtra 400710
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
